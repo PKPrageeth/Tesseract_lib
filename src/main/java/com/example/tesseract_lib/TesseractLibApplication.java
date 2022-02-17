8c05f89dc20585dbbb9c598e38c5274a2d50b430
CVS <pitigalaksanjeewa@gmail.com>
2022-02-17 10:16:51 +0530

    first

---
+++ src/main/java/com/example/tesseract_lib/TesseractLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.tesseract_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesseractLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesseractLibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tesseract_lib/TesseractLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tesseract_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesseractLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
